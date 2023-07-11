1a4023dc3b1e891990e3c763d397afa0ae8109d9
Caua Ribeiro <cauarbeiro30@gmail.com>
2023-07-11 17:41:39 -0300

    Game sedd

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -15,22 +15,27 @@ public class Game {
     @Column(name = "game_year")// alterando o nome para não dar conflito com o bando de dados
     private Integer year;
     private String genre;
-    private String platform;
-    private String imUrl;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    @Column (columnDefinition = "TEXT") // instrui a JPA a tirar a restição do VARCHAR(255) e definir como texto
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
+                String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
-        this.imUrl = imUrl;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -67,20 +72,28 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public Double getScore() {
+        return score;
     }
 
-    public String getImUrl() {
-        return imUrl;
+    public void setScore(Double score) {
+        this.score = score;
     }
 
-    public void setImUrl(String imUrl) {
-        this.imUrl = imUrl;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
